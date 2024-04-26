f725d2cc663ce209b297003593a4eed13467edc0
Valeri Panteleev <valeri.panteleev@fabit.ru>
2024-04-26 05:46:00 +0300

    fix: правка в dataNormalize

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -5,9 +5,6 @@ export const dataNormalize = (data: TMarketResponse[]): TMarketData => {
 
   data.forEach((res) => {
     Object.keys(res.rates).forEach((currency) => {
-      if (!res.base) {
-        return;
-      }
       const ticker = `${currency}/${res.base}`;
       if (!result[ticker]) {
         result[ticker] = [];
